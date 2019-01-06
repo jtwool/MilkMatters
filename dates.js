0df6a5fd13e2213ef1e8a811bb8863d02c3b4875
JT W <jwolohan@gmail.com>
2019-01-06 12:45:32 -0500

    UPDATE Saturday dates in JS

--- dates.js
+++ dates.js
@@ -5,6 +5,33 @@ var date_options = { weekday: 'long', year: 'numeric', month: 'long', day: 'nume
 // Months start counting at 0
 // So January = 0, February = 1 ... December = 11
 
+saturday_dates = [
+// January
+new Date(YEAR, 0, 16),
+// February
+new Date(YEAR, 1, 2),
+// March
+new Date(YEAR, 2, 2),
+// April
+new Date(YEAR, 3, 6),
+// May
+new Date(YEAR, 4, 4),
+// June
+new Date(YEAR, 5, 1),
+// July
+new Date(YEAR, 6, 6),
+// August
+new Date(YEAR, 7, 3),
+// September
+new Date(YEAR, 8, 7),
+// October
+new Date(YEAR, 9, 5),
+// November
+new Date(YEAR, 10, 2),
+// December
+new Date(YEAR, 11, 7),
+]
+
 evening_dates = [
 // Dec 18
 new Date(2018, 11, 19),
@@ -76,18 +103,30 @@ new Date(YEAR, 11, 11),
 // new Date(YEAR, 11, 26),
 ]
 
-today = new Date()
+var get_next_days = function(x,n=4){
+  x.filter(y=>y>new Date()).slice(0,n);
+  return x
+}
 
-next_four_mornings = morning_dates.filter(x=>x>today).slice(0,4)
-next_four_evenings = evening_dates.filter(x=>x>today).slice(0,4)
+next_four_saturday = get_next_days(saturday_dates)
+next_four_mornings = get_next_days(morning_dates)
+next_four_evenings = get_next_days(evening_dates)
 
-if (next_four_evenings[0] < next_four_mornings[0]){
+first_next_days = [next_four_saturday[0],
+                   next_four_mornings[0],
+                   next_four_evenings[0]]
+
+if (next_four_evenings[0] ==  min(first_next_days)){
   next_date = next_four_evenings[0]
   next_loc = '<a href="https://goo.gl/maps/tQGhVUdfHuo" target="_blank">  City Hall in McCloskey Room 135</a> from 6:30pm-8pm'
-} else{
+} else if (next_four_mornings[0] == min(first_next_days)){
   next_date = next_four_mornings[0]
   next_loc= '<a href="https://goo.gl/maps/xEkWaSfFatR2" target="_blank">  Banneker Community Center</a> from 9:30am-11:30am'
 }
+  else{
+    next_date = next_four_saturday[0]
+    next_loc= '<a href="https://goo.gl/maps/LM4CWyKSKn92" target="_blank">  Crestmont Boys &amp; Girls Club</a> from 2:00-4:00pm'
+  }
 
 nds = next_date.toLocaleDateString("en-US",date_options)
 
@@ -96,7 +135,8 @@ document.getElementById("next_loc").innerHTML = next_loc
 
 n4m = next_four_mornings.map(x=>"<li>"+ x.toLocaleDateString("en-US",date_options) + "</li>").join(" ")
 n4e = next_four_evenings.map(x=>"<li>"+ x.toLocaleDateString("en-US",date_options) + "</li>").join(" ")
-
+n4s = next_four_saturday.map(x=>"<li>"+ x.toLocaleDateString("en-US",date_options) + "</li>").join(" ")
 
 document.getElementById("next-four-morning").innerHTML = n4m
 document.getElementById("next-four-evening").innerHTML = n4e
+document.getElementById("next-four-saturday").innerHTML = n4s
